2c8c973c6b9fec3ae50f8f7e63ce58baa68af13a
Nelson Rotimi <nelsonabieno@gmail.com>
2022-05-30 06:34:10 +0100

    refactor tests

--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -1,15 +1,18 @@
 require_relative '../cash_register'
+require_relative '../app/product_store'
 
 RSpec.describe CashRegister do
+  let (:products) {ProductStore.get_products('./data/product_store.json')}
+
   context "adding products GR1,GR1 to basket should calculate total price" do
-    it { expect(CashRegister.get_items_to_basket("GR1,GR1")).to eql '3.11€' }
+    it { expect(CashRegister.get_items_to_basket("GR1,GR1", products)).to eql '3.11€' }
   end
 
   context "adding products SR1,SR1,GR1,SR1 should calculate total price" do
-    it { expect(CashRegister.get_items_to_basket("SR1,SR1,GR1,SR1")).to eql '16.61€' }
+    it { expect(CashRegister.get_items_to_basket("SR1,SR1,GR1,SR1", products)).to eql '16.61€' }
   end
 
   context "adding products GR1,CF1,SR1,CF1,CF1 should calculate total price" do
-    it { expect(CashRegister.get_items_to_basket("GR1,CF1,SR1,CF1,CF1")).to eql '30.57€' }
+    it { expect(CashRegister.get_items_to_basket("GR1,CF1,SR1,CF1,CF1", products)).to eql '30.57€' }
   end
 end
