16280e12c04c09aef9a1eff21b1be972dc709839
Nelson Rotimi <nelsonabieno@gmail.com>
2022-05-30 05:10:32 +0100

    sort items in basket by product code

---
+++ app/basket.rb
@@ -0,0 +1,26 @@
+require_relative 'product_store'
+require_relative 'price'
+
+class Basket
+  # sort items by the product code
+  def self.sort_items(basket)
+    unknown_products = []
+    product_count = {}
+    product_hash = ProductStore.get_products('./data/product_store.json')
+
+    basket.each do |item|
+      code = item.upcase
+      if product_hash.has_key? code
+        if product_count.has_key? code
+          product_count[code] += 1
+        else
+          product_count[code] = 1
+        end
+      else
+        unknown_products << code
+      end
+    end
+
+    Price.calculate(product_count)
+  end
+end
